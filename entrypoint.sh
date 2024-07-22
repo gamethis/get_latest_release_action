fdba08ab7abc55edc0a292d2180d7fcde5bd2a88
Larry <34108925+gamethis@users.noreply.github.com>
2024-07-22 18:54:31 -0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -l
+
+REPO=$1
+DESIRED_VERSION=$2
+
+function getLatestRepoVersion() {
+  LATEST_ARR=($(wget -q -O- https://api.github.com/repos/${REPO}/releases 2> /dev/null | awk '/tag_name/ { print $2 }' | cut -d '"' -f 2 | cut -d 'v' -f 2 | sort -V -r))
+  for ver in "${LATEST_ARR[@]}"; do
+    if [[ -n "${DESIRED_VERSION}" ]]; then
+      if [[ $ver =~ $DESIRED_VERSION ]] && [[ ! $ver =~ beta ]] && [[ ! $ver =~ rc ]] && [[ ! $ver =~ alpha ]] ; then
+        LATEST="$ver"
+        break
+      fi
+    else
+      if [[ ! $ver =~ beta ]] && [[ ! $ver =~ rc ]] && [[ ! $ver =~ alpha ]] ; then
+        LATEST="$ver"
+        break
+      fi
+    fi
+  done
+  echo -n "$LATEST"
+}
